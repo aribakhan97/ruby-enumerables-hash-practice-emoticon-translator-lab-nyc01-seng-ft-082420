511629ae6e83890f5d17510b9245980a1579f8e1
Ariba Khan <akhan41@binghamton.edu>
2020-08-17 02:11:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,11 @@ end
 
 def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
-  english_meaning = emoticon_lib['get_meaning'][emoticon]
-  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
-  # code goes here
+  emoticon_lib.each do |key|
+    #binding.pry
+    if key[1][:japanese] == emoticon
+      return key[0]
+    end
+  end
+  return 'Sorry, that emoticon was not found'
 end
