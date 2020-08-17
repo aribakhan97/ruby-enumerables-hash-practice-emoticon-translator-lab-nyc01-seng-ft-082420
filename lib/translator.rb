39522e02a70d419795d6eeb731dd4f5ab98110f6
Ariba Khan <akhan41@binghamton.edu>
2020-08-17 02:07:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(emoticon_file, emoticon)
   emoticon_lib.each do |key|
     #binding.pry
     if key[1][:english] == emoticon
-      return key[:japanese]
+      return key[1][:japanese]
     end
   end
   return 'Sorry, that emoticon was not found'
